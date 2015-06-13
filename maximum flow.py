5265b103146835b9f388f0f0e9f53d3488579ee3
Hane Liu <hane0131@gmail.com>
2015-06-14 01:01:31 +0800

    Declare new function edmonds_karp to calculate maximum flow

--- maximum flow.py
+++ maximum flow.py
@@ -12,6 +12,7 @@ class Edge(object):
 def breadth_first_search(graph, source, sink):
     queue = [source]
     path = {source: []}
+
     while queue:
         src = queue.pop(0)
         for dest in range(len(graph)):
@@ -20,7 +21,24 @@ def breadth_first_search(graph, source, sink):
                 if dest == sink:
                     return path[dest]
                 queue.append(dest)
+
     return None
+
+
+def edmonds_karp(graph, source, sink):
+    max_flow = 0
+
+    while True:
+        path = breadth_first_search(graph, source, sink)
+        if not path:
+            break
+        flow = min(graph[i][j] for i, j in path)
+        for i, j in path:
+            graph[i][j] -= flow
+            graph[j][i] += flow
+        max_flow += flow
+
+    return max_flow
 
 
 def main():
