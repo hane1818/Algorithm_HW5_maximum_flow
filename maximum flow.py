02232802a58f4e4dc178166eda728e7a8e28894f
Hane Liu <hane0131@gmail.com>
2015-06-14 00:47:58 +0800

    Comment out class Edge & declare new function to do BFS

--- maximum flow.py
+++ maximum flow.py
@@ -1,11 +1,26 @@
 import sys
 
-
+"""
 class Edge(object):
     def __init__(self, src, dest, flow):
         self.src = src
         self.dest = dest
         self.max_flow = flow
+"""
+
+
+def breadth_first_search(graph, source, sink):
+    queue = [source]
+    path = {source: []}
+    while queue:
+        src = queue.pop(0)
+        for dest in range(len(graph)):
+            if graph[src][dest] > 0 and dest not in path:
+                path[dest] = path[src] + {(src, dest)}
+                if dest == sink:
+                    return path[dest]
+                queue.append(dest)
+    return None
 
 
 def main():
